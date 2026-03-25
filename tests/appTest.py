e0ca003409946aba91386b4033b7f6b509a2fd5c
Daniel Vasconcelos <79517613+danbasco@users.noreply.github.com>
2026-03-25 22:34:50 +0000

    feat/test_hello

--- tests/appTest.py
+++ tests/appTest.py
@@ -10,3 +10,14 @@ class AppTest(unittest.TestCase):
         self.assertEqual(200, response.status_code, "Erro no test_http_code!")
         self.assertEqual("<h1>Hello, I'm Alive!</h1>", response.get_data(as_text=True)
                           , "Erro no test_print_health_check!")
+
+    def test_print_hello_sucess(self):
+
+        response = self.app.get('/hello?name=Daniel')
+        self.assertEqual(200, response.status_code)
+        self.assertEqual("Hello, Daniel!", response.get_data(as_text=True))
+
+    def test_print_hello_error(self):
+
+        response = self.app.get('/hello')
+        self.assertEqual(400, response.status_code)
